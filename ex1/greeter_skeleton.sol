a7a2e030edd6048fac92f21b61fa2277c3bfbdcd
Jack Vasylenko <vasylenko@berkeley.edu>
2017-09-15 10:12:31 -0700

    Finished hw1
    Done

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -4,12 +4,18 @@ pragma solidity ^0.4.13;
 contract greeter {
 	/* Add one variable to hold our greeting */
 	string greeting;
+	uint public count;
+
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
+		greeting = _greeting;
+		count = 0;
 	}
 
 	function greet() constant returns (string)  {
 		/* Write one line of code to allow the contract to return our greeting */
+		count = count + 1;
+		return greeting;
 	}
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -3,8 +3,18 @@ pragma solidity ^0.4.16;
 
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+		uint x = 0;
+		uint y = 1;
+		if (position == 0)
+			return x;
+		for (uint i = 2; i <= position; i++) {
+			uint z = x + y;
+			x = y;
+			y = z;
+		}
+		return y;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function () {}
 }
